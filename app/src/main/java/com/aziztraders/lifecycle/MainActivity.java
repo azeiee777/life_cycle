1965a3cde98bb15dcbf5653f30ad2a8c9f0caa09
Abdul <Abdul002ajij@gmail.com>
2022-07-13 09:17:31 +0530

    activity and fragment lifecycle implemented

--- app/src/main/java/com/aziztraders/lifecycle/MainActivity.java
+++ app/src/main/java/com/aziztraders/lifecycle/MainActivity.java
@@ -34,4 +34,34 @@ public class MainActivity extends AppCompatActivity {
                 ft.replace(R.id.fragment_container,mf,"mainFragment")
                 .commit();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
+    }
 }
